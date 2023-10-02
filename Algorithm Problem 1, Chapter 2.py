9a99231af224d01165d9ce79c8bf2581de63879e
AceOfSpades <96136812+AceOfSpadesI@users.noreply.github.com>
2023-10-02 15:48:16 -0400

    Add files via upload

---
+++ Algorithm Problem 1, Chapter 2.py
@@ -0,0 +1,11 @@
+#Algorithm Workbench
+#Problem 1: Write Python code that prompts the user to enter his or her height and assigns the user's input to a variable named *height*.
+
+
+#First, we have to create a input for the user to put down their favorite color. To do this, we use an input function.
+#We then set the results of the user to the variable "color", so we can use the results as we please.
+#Then, to confirm that the program worked, I used a print function to output the result of what we put into the program.
+#In essence, we are entering an input, and expecting an output that is the same as the input.
+
+color = input("What color do you want?")
+print(color)
