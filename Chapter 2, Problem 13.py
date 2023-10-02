f88477b681a7c5cc121d64b00e3cf864c773c61e
AceOfSpades <96136812+AceOfSpadesI@users.noreply.github.com>
2023-10-02 16:24:37 -0400

    Update Chapter 2, Problem 13.py

--- Chapter 2, Problem 13.py
+++ Chapter 2, Problem 13.py
@@ -1,10 +1,10 @@
 # User inputs the parameters for the grapevine. Each of these inputs is necessary to run the equation that is defined below. They were named more bluntly to ensure clarity.
 
-user_length = int(input("Input the length of the vineyard here in feet:")) # Our "R" variable
-user_space = int(input("Input the amount of space used in feet by an end post assembly here:")) # Our "E" variable
-user_vines = int(input("Input the space in between the vines in feet here:")) # Our "S" variable
+user_length = int(input("Input the length of the vineyard here in feet:")) # Our "R" variable.
+user_space = int(input("Input the amount of space used in feet by an end post assembly here:")) # Our "E" variable.
+user_vines = int(input("Input the space in between the vines in feet here:")) # Our "S" variable.
 
-# Equation that is used in order to calculate the total number of grapevines that would fit. grapevine_total is the "V" variable that was found in the problem
+# Equation that is used in order to calculate the total number of grapevines that would fit. grapevine_total is the "V" variable that was found in the problem.
 
 grapevine_total = (user_length - (2 * user_space)) / user_vines
 
@@ -15,7 +15,7 @@ rounded_grapevines = round(grapevine_total)
 # I added an if statement to create a realistic result for the grapevines, as having negative grapevines makes no sense.
 
 if rounded_grapevines > 0:
-    print(f"The number of grapevines that will fit here in the row is {rounded_grapevines}")
+    print(f"The number of grapevines that will fit here in the row is {rounded_grapevines}") # If the resulting number is above zero, the result is posted into the console.
 if rounded_grapevines < 0:
-    print("You can't fit any grapevines here, try again!")
+    print("You can't fit any grapevines here, try again!") # If the resulting number is a negative number, the result is not posted into the console.
 
