76eba37827e09c5b3930cb5922bdf41a778535b3
AceOfSpades <96136812+AceOfSpadesI@users.noreply.github.com>
2023-10-16 21:49:46 -0400

    Update Algorithm Workbench 1, while loop.py

--- Chapter4/Algorithm Workbench 1, while loop.py
+++ Chapter4/Algorithm Workbench 1, while loop.py
@@ -1,1 +1,12 @@
+#Algorithm Workbench, Question 1
 
+
+#Product, as defined in the problem, has to be set to zero in order for the loop to work.
+product = 0
+#Create the user input.
+user_input = int(input("Number you want multiplied here: "))
+#While loop executes the function until the number hits 100
+while product < 100:
+    product = user_input * 10
+#Print result
+print(product)
